10ba38e7f18308e7dbb63dbb85e4b4b47bb712bf
opawlat <pawlat.olga@gmail.com>
2025-03-26 20:08:22 +0100

    Update config.ts
    delete every commented #

--- app/config.ts
+++ app/config.ts
@@ -7,9 +7,6 @@ export const metaData = {
 };
 
 export const socialLinks = {
-  #twitter: "https://x.com/1tssirius",
-  #github: "https://github.com/1msirius/Nextfolio",
-  #instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/",
   email: "mailto:pawlat.olga@gmail.com",
 };
