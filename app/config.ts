48db888d9ac7082744655ad28a8ba66a56889a92
opawlat <pawlat.olga@gmail.com>
2025-03-26 20:04:16 +0100

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,6 @@
 export const metaData = {
   baseUrl: "https://olgapawlat.vercel.app/",
   title: "Olga Pawłat",
-  #name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
