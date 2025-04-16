bbaddef59125790df5011a5a9ef82fe62c9517ec
opawlat <pawlat.olga@gmail.com>
2025-04-16 19:17:19 +0200

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,12 +1,16 @@
 export const metaData = {
   baseUrl: "https://olgapawlat.vercel.app/",
   title: "Olga Pawłat",
+  name: "Olga Pawłat",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Rozwijam produkty IT",
 };
 
 export const socialLinks = {
+  twitter: "https://x.com/1tssirius",
+  github: "https://github.com/1msirius/Nextfolio",
+  instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/olgapawlat",
   email: "mailto:pawlat.olga@gmail.com",
 };
